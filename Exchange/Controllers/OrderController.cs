2e5f61e805faacff1f15f1669298efc849d5808e
EAValov <eugene.valov@gmail.com>
2015-02-04 16:04:58 +0500

    Finaly

--- Exchange/Controllers/OrderController.cs
+++ Exchange/Controllers/OrderController.cs
@@ -26,75 +26,86 @@ namespace Exchange.Controllers
         {
             return repository.SellOrders;
         }
-        public IEnumerable<CompletedOrder> GetCompletedOrderOrders()
+        public IEnumerable<CompletedOrder> GetCompletedOrders()
         {
             return repository.CompletedOrders;
         }
 
-        //public async Task<HttpResponseMessage> PostBuyOrder(BuyOrder buyorder)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        await repository.SaveBuyOrderAsync(buyorder);
+        public async Task<HttpResponseMessage> PostBuyOrder(BuyOrder buyorder)
+        {
+            if (ModelState.IsValid)
+            {
+                buyorder.CreatedAt = DateTime.Now;
+                await repository.SaveBuyOrderAsync(buyorder);
 
-        //        HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, buyorder);
-        //        response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = buyorder.BuyOrderID }));
-        //        await CompletedOrderAdded();
-        //        return response;
-        //    }
-        //    else
-        //    {
-        //        return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
-        //    }
-        //}
-        //public async Task<HttpResponseMessage> PostSellOrder(SellOrder sellorder)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        await repository.SaveSellOrderAsync(sellorder);
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, buyorder);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = buyorder.BuyOrderID }));
+                await CompletedOrderAdded();
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+        public async Task<HttpResponseMessage> PostSellOrder(SellOrder sellorder)
+        {
+            if (ModelState.IsValid)
+            {
+                sellorder.CreatedAt = DateTime.Now;
+                await repository.SaveSellOrderAsync(sellorder);
 
-        //        HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, sellorder);
-        //        response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = sellorder.SellOrderID }));
-        //        await CompletedOrderAdded();
-        //        return response;
-        //    }
-        //    else
-        //    {
-        //        return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
-        //    }
-        //}
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, sellorder);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = sellorder.SellOrderID }));
+                await CompletedOrderAdded();
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
 
-        //public async Task CompletedOrderAdded()
-        //{
-        //    foreach (var sellorder in repository.SellOrders)
-        //    {
-        //        foreach (var buyorder in repository.BuyOrders)
-        //        {
-        //            if (sellorder.Price <= buyorder.Price)
-        //            {
-        //                CompletedOrder completedOrder = new CompletedOrder()
-        //                {
-        //                    BuyOrderDate = buyorder.CreatedAt,
-        //                    SellOrderDate = sellorder.CreatedAt,
-        //                    Price = sellorder.Price,
-        //                    CompletedAt = DateTime.Now,
-        //                    BuyComment = buyorder.Commentary,
-        //                    SellComment = sellorder.Commentary,
-        //                };
-        //                while (sellorder.Quantity >= 0 || buyorder.Quantity >= 0)
-        //                {
-        //                    sellorder.Quantity--;
-        //                    buyorder.Quantity--;
-        //                    completedOrder.Quantity++;
-        //                }
+        public async Task CompletedOrderAdded()
+        {
+            //toList because enumerator will not allow to save entity to db
+            foreach (var sellorder in repository.SellOrders.Where(q=>q.Quantity > 0).ToList())
+            {
+                foreach (var buyorder in repository.BuyOrders.ToList().Where(q => q.Quantity > 0))
+                {
+                    if (sellorder.Price <= buyorder.Price)
+                    {
+                        CompletedOrder completedOrder = new CompletedOrder()
+                        {
+                            BuyOrderDate = buyorder.CreatedAt,
+                            SellOrderDate = sellorder.CreatedAt,
+                            Price = sellorder.Price,
+                            CompletedAt = DateTime.Now,
+                            BuyComment = buyorder.Commentary,
+                            SellComment = sellorder.Commentary,
+                        };
 
-        //                await repository.SaveBuyOrderAsync(buyorder);
-        //                await repository.SaveSellOrderAsync(sellorder);
-        //                await repository.SaveCompletedOrderAsync(completedOrder);
-        //            }
-        //        }
-        //    }
+                        //quantity accounting(because we can't sell negative number of items)
+                        if (buyorder.Quantity >= sellorder.Quantity)
+                        {
+                            completedOrder.Quantity = sellorder.Quantity;
+                            buyorder.Quantity -= sellorder.Quantity;
+                            sellorder.Quantity = 0;
+                        }
+                        else
+                        {
+                            sellorder.Quantity -= buyorder.Quantity;
+                            completedOrder.Quantity = buyorder.Quantity;
+                            buyorder.Quantity = 0;
+                        }
 
-        //}
+                        await repository.SaveBuyOrderAsync(buyorder);
+                        await repository.SaveSellOrderAsync(sellorder);
+                        await repository.SaveCompletedOrderAsync(completedOrder);
+                    }
+                }
+            }
+
+        }
     }
 }
